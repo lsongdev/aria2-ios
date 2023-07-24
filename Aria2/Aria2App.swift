3de37f9e1847fbf4b526e92c3090fefc046b3902
Lsong <song940@gmail.com>
2023-07-24 19:48:58 +0800

    Initial Commit

---
+++ Aria2/Aria2App.swift
@@ -0,0 +1,17 @@
+//
+//  Aria2App.swift
+//  Aria2
+//
+//  Created by Lsong on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Aria2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
